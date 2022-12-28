2ccfc4ae80080428df96c0490d1c552751eff107
yogendra.londhe <yogendra.londhe@gmail.com>
2022-12-28 18:20:45 +0530

    added code in branch 2

---
+++ Testing/src/test/Reverse.java
@@ -0,0 +1,26 @@
+package test;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String s="yogi";
+ char[] ch=s.toCharArray();
+  String rev = null;
+  
+  for(int i=ch.length-1;i>=0;i--) {
+	 // System.out.println(ch[i]);
+	 // rev= s.charAt(i)+rev;
+	  System.out.print(s.charAt(i));
+	  if(i!=(s.length()-1))
+	  {
+		  char b =ch[i] ;
+	        char ch1;  
+	     /*   ch1 = b[i];  
+	        b[i] = b[j];  
+	        b[j] = ch1;  */
+		  
+  }
+  }}}
+
+

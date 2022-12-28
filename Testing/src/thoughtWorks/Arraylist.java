4c57c712ae4cfb6e0ded1f332458b94fbde65880
yogendra.londhe <yogendra.londhe@gmail.com>
2022-12-28 17:49:23 +0530

    newCodedAdded

---
+++ Testing/src/thoughtWorks/Arraylist.java
@@ -0,0 +1,21 @@
+package thoughtWorks;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*	Arraylist a = ["1","2","abc","bcd"," 5"];
+	    for(int i=0 ; i<=a.length(); i++){
+	        
+	    
+	    }*/
+
+	}
+
+	private int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
